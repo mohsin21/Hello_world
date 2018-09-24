837adff86758af0b2c8f14e1333cb71e7d648940
mohsin21 <42766450+mohsin21@users.noreply.github.com>
2018-09-24 14:57:44 +0530

    Create code7.java

---
+++ code7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int n;
+		n = in.nextInt();
+		String str = "hello";
+		int i = 1;
+		while( i<=n )
+		{
+		    System.out.println(str);
+		    i++;
+		}
+		
+	}
+}
