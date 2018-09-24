11f63be0bd1e8ca2b721a7a6f9168c2c96bb124b
mohsin21 <42766450+mohsin21@users.noreply.github.com>
2018-09-24 14:39:30 +0530

    Create code5.java

---
+++ code5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int x,y,z;
+		x = in.nextInt();
+		y = in.nextInt();
+		z = in.nextInt();
+		if(x>y && x>z)
+		{
+		    System.out.println(x);
+		}
+		else if(y>x && y>z)
+		{
+		    System.out.println(y);
+		}
+		else
+		System.out.println(z);
+	}
+}
