fc2906d017b8ee009697b653375f1266e07b110c
mohsin21 <42766450+mohsin21@users.noreply.github.com>
2018-09-24 14:42:59 +0530

    Create code6.java

---
+++ code6.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    int x,y,z;
+		x = in.nextInt();
+		if(x%4 == 0)
+		{
+		    System.out.println("yes");
+		}
+		else
+		System.out.println("no");
+	}
+}
