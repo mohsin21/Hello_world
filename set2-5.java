509f7fe45cd70c8979464bedf5b6d2b7ef76a131
mohsin21 <42766450+mohsin21@users.noreply.github.com>
2018-12-18 10:43:09 +0530

    Create set2-5.java

---
+++ set2-5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{    
+ public static void main(String args[])
+ { 
+   Scanner in=new Scanner(System.in);   
+   int n=in.nextInt();
+   int m=in.nextInt();
+   for(int i=n;i<m;i++)
+   {
+     if((i)%2==0)
+     {
+       System.out.println(i);
+     }
+     else
+     {
+       System.out.println();
+     }
+   }
+  }
+}
