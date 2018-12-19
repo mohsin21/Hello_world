14f94cbb451e530c4b05043e8c537b19b6b0fa82
mohsin21 <42766450+mohsin21@users.noreply.github.com>
2018-12-19 11:08:20 +0530

    Create set2-8.java

---
+++ set2-8.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+      Scanner in=new Scanner(System.in);
+
+        int low = in.nextInt();
+        int high = in.nextInt();
+
+        for(int number = low + 1; number < high; ++number) {
+            int digits = 0;
+            int result = 0;
+            int originalNumber = number;
+
+            // number of digits calculation
+            while (originalNumber != 0) {
+                originalNumber /= 10;
+                ++digits;
+            }
+
+            originalNumber = number;
+
+            // result contains sum of nth power of its digits
+            while (originalNumber != 0) {
+                int remainder = originalNumber % 10;
+                result += Math.pow(remainder, digits);
+                originalNumber /= 10;
+            }
+
+            if (result == number)
+                System.out.print(number + " ");
+        }
+    }
+}
